5562dce8c360b29e1d05dc50d5c8d5d67eea4f02
santiago_valenzuela <svalenzuela@nexura.com>
2026-02-17 15:43:08 -0500

    Correccion de nombre dominio->origen en models.py

--- models.py
+++ models.py
@@ -1,18 +1,21 @@
 from sqlalchemy import Column, Integer, String, DateTime, BigInteger, Float
 from sqlalchemy.ext.declarative import declarative_base
-from datetime import datetime
+from datetime import datetime,timezone
 
 Base = declarative_base()
 
 class DescargaAuditoria(Base):
     __tablename__ = "tn_descargas_auditoria"
 
-    id = Column(Integer, primary_key=True, index=True)
-    usuario_id = Column(Integer, nullable=True)
-    ip = Column(String(45), index=True)
-    recurso = Column(String(500))  # El path en el NFS
-    estado = Column(String(20), default="PENDING") # PENDING, REDIRECTED, COMPLETED, FAILED
-    duracion_ms = Column(Float, default=0.0)
-    tamano_bytes = Column(BigInteger, default=0)
-    dominio = Column(String(255))
-    fecha_actualizacion = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    fecha = Column(DateTime, default=datetime.utcnow) # Campo 2 de tu imagen
+    ip = Column(String(255))                          # Campo 3
+    recurso = Column(String(500))                    # Campo 4
+    mime = Column(String(50))                         # Campo 5
+    usuario_id = Column(Integer, nullable=True)       # Campo 6
+    origen = Column(String(50), nullable=False)       # Campo 7 
+    user_agent = Column(String(350), nullable=True)   # Campo 8
+    estado = Column(String(50), default="PENDING")    # Campo 9
+    tamano_bytes = Column(BigInteger, default=0)      # Campo 11
+    duracion_ms = Column(Integer, default=0)          # Campo 12
+    fecha_actualizacion = Column(DateTime, default=datetime.utcnow, onupdate=datetime.now(timezone.utc))
